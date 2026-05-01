7dd2736ebb4db4db5f62fa371c71dd422713c1e1
Chris Lacy <chris@actionlauncher.com>
2026-05-02 07:31:20 +1000

    fix: allow airtouch skills to resolve devices automatically

--- skills/au.com.polyaire.airtouch5.set-home-controls/scripts/run.js
+++ skills/au.com.polyaire.airtouch5.set-home-controls/scripts/run.js
@@ -3,18 +3,12 @@
 const {
   parseHomeControlsArgs,
   runHomeControlsSkill,
+  splitDeviceAndArgs,
 } = require("../../utils/airtouch5_home_controls.js");
 
 const skillId = "au.com.polyaire.airtouch5.set-home-controls";
 const argvArgs = process.argv.slice(2);
-let deviceId = process.env.CLAWPERATOR_DEVICE_ID || "";
-let rawArgs = argvArgs;
-if (!deviceId && rawArgs[0] && !rawArgs[0].startsWith("--")) {
-  deviceId = rawArgs[0];
-  rawArgs = rawArgs.slice(1);
-} else if (deviceId && rawArgs[0] === deviceId) {
-  rawArgs = rawArgs.slice(1);
-}
+const { deviceId, rawArgs } = splitDeviceAndArgs(argvArgs, process.env.CLAWPERATOR_DEVICE_ID || "");
 const parsed = parseHomeControlsArgs(rawArgs);
 
 runHomeControlsSkill({
--- skills/au.com.polyaire.airtouch5.set-power-state/scripts/run.js
+++ skills/au.com.polyaire.airtouch5.set-power-state/scripts/run.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const { parseChoiceArg, runPowerStateSkill } = require("../../utils/airtouch5_home_controls.js");
+const { parseChoiceArg, runPowerStateSkill, splitDeviceAndArgs } = require("../../utils/airtouch5_home_controls.js");
 
 const skillId = "au.com.polyaire.airtouch5.set-power-state";
-const deviceId = process.env.CLAWPERATOR_DEVICE_ID || process.argv[2] || "";
-const rawArgs = process.env.CLAWPERATOR_DEVICE_ID ? process.argv.slice(2) : process.argv.slice(3);
+const argvArgs = process.argv.slice(2);
+const { deviceId, rawArgs } = splitDeviceAndArgs(argvArgs, process.env.CLAWPERATOR_DEVICE_ID || "");
 const requestedState = parseChoiceArg(rawArgs, { flag: "--state", allowedValues: ["on", "off"] });
 
 runPowerStateSkill({
